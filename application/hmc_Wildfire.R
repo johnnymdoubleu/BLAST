437bd072ca4127c375a6604a2a0d3d3aaeb6958f
Johnny <37770895+johnnymdoubleu@users.noreply.github.com>
2023-10-11 21:32:21 +0100

    Update hmc_Wildfire.R

--- application/hmc_Wildfire.R
+++ application/hmc_Wildfire.R
@@ -267,7 +267,7 @@ fit1 <- stan(
     # init_r = 1,
     chains = 3,             # number of Markov chains
     warmup = 1000,          # number of warmup iterations per chain
-    iter = 2000,            # total number of iterations per chain
+    iter = 3000,            # total number of iterations per chain
     cores = 4,              # number of cores (could use one per chain)
     refresh = 1             # no progress shown
 )
@@ -620,3 +620,4 @@ ppc_loo_pit_overlay(
     yrep = yrep,
     lw = weights(fwi.loo$psis_object)
 )
+waic(posterior$log_lik)
