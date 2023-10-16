82a7326b279e010f980bea6aee15b1e9f5d970c5
Johnny <37770895+johnnymdoubleu@users.noreply.github.com>
2023-10-16 01:26:26 +0100

    Update hmc_Wildfire.R

--- application/hmc_Wildfire.R
+++ application/hmc_Wildfire.R
@@ -217,7 +217,7 @@ model {
     for (i in 1:n){
         target += pareto_lpdf(y[i] | u, alpha[i]);
     }
-    target += gamma_lpdf(lambda1 | 1, 7);
+    target += gamma_lpdf(lambda1 | 1, 5.5);
     target += gamma_lpdf(lambda2 | 0.1, 0.1);
     target += inv_gamma_lpdf(sigma | 0.01, 0.01);
     target += double_exponential_lpdf(theta[1] | 0, lambda1); // target += normal_lpdf(theta[1] | 0, 0.1);
