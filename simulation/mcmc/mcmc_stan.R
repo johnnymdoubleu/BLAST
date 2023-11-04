716135731e8b5418772a5a46c5cdcb6b4d05f5fc
Johnny <37770895+johnnymdoubleu@users.noreply.github.com>
2023-11-04 16:22:40 +0000

    Update

--- simulation/mcmc/mcmc_stan.R
+++ simulation/mcmc/mcmc_stan.R
@@ -22,43 +22,43 @@ library(cmdstanr)
 # library(ggplotify)
 
 #Scenario 1
-# set.seed(2)
-set.seed(50)
+set.seed(2)
+# set.seed(50)
 
-n <- 5000
-psi <- 20
-threshold <- 0.90
-p <- 10
-no.theta <- 1
-simul.no <- 50
+# n <- 5000
+# psi <- 20
+# threshold <- 0.90
+# p <- 10
+# no.theta <- 1
+# simul.no <- 50
 
-xholder.nonlinear <- xholder.linear <- bs.nonlinear <- bs.linear <- matrix(,nrow=n, ncol=0)
-x.origin <- cbind(replicate(p, runif(n, 0, 1)))
-for(i in 1:p){
-    knots <- seq(min(x.origin[,i]), max(x.origin[,i]), length.out = psi)  
-    tps <- basis.tps(x.origin[,i], knots, m = 2, rk = FALSE, intercept = FALSE)
-    # tps <- mSpline(x.origin[,i], df=psi, Boundary.knots = range(x.origin[,i]), degree = 3, intercept=TRUE)
-    #   bs.x <- cbind(bs.x, tps)
-    bs.linear <- cbind(bs.linear, tps[,1:no.theta])
-    bs.nonlinear <- cbind(bs.nonlinear, tps[,-c(1:no.theta)])  
-}
+# xholder.nonlinear <- xholder.linear <- bs.nonlinear <- bs.linear <- matrix(,nrow=n, ncol=0)
+# x.origin <- cbind(replicate(p, runif(n, 0, 1)))
+# for(i in 1:p){
+#     knots <- seq(min(x.origin[,i]), max(x.origin[,i]), length.out = psi)  
+#     tps <- basis.tps(x.origin[,i], knots, m = 2, rk = FALSE, intercept = FALSE)
+#     # tps <- mSpline(x.origin[,i], df=psi, Boundary.knots = range(x.origin[,i]), degree = 3, intercept=TRUE)
+#     #   bs.x <- cbind(bs.x, tps)
+#     bs.linear <- cbind(bs.linear, tps[,1:no.theta])
+#     bs.nonlinear <- cbind(bs.nonlinear, tps[,-c(1:no.theta)])  
+# }
 
-gamma.origin <- matrix(, nrow = psi, ncol = p)
-for(j in 1:p){
-    for (ps in 1:psi){
-        if(j %in% c(2,4,5,6,9,10)){gamma.origin[ps, j] <- 0}
-        else if(j==7){
-            if(ps <= (psi/2)){gamma.origin[ps, j] <- 1}
-            else{gamma.origin[ps, j] <- 1}
-        }
-        else {
-            if(ps <= (psi/2)){gamma.origin[ps, j] <- 1}
-            else{gamma.origin[ps, j] <- 1}
-        }
-    }
-}
+# gamma.origin <- matrix(, nrow = psi, ncol = p)
+# for(j in 1:p){
+#     for (ps in 1:psi){
+#         if(j %in% c(2,4,5,6,9,10)){gamma.origin[ps, j] <- 0}
+#         else if(j==7){
+#             if(ps <= (psi/2)){gamma.origin[ps, j] <- 1}
+#             else{gamma.origin[ps, j] <- 1}
+#         }
+#         else {
+#             if(ps <= (psi/2)){gamma.origin[ps, j] <- 1}
+#             else{gamma.origin[ps, j] <- 1}
+#         }
+#     }
+# }
 
-theta.origin <- c(-0.1, 0.8, 0, 0.8, 0, 0, 0, -0.3, 0.8, 0, 0)
+# theta.origin <- c(-0.1, 0.8, 0, 0.8, 0, 0, 0, -0.3, 0.8, 0, 0)
 
 n <- 50000
 psi <- 20
@@ -242,7 +242,7 @@ model {
         target += pareto_lpdf(y[i] | u, alpha[i]);
     }
     target += gamma_lpdf(lambda1 | 1, 10);
-    target += gamma_lpdf(lambda2 | 1, 10);
+    target += gamma_lpdf(lambda2 | 0.1, 1.5);
     target += normal_lpdf(theta[1] | 0, square(100));
     target += inv_gamma_lpdf(sigma | 0.01, 0.01); // target += double_exponential_lpdf(theta[1] | 0, lambda1)
     target += (p * log(lambda1) + (p*psi) * log(lambda2));
