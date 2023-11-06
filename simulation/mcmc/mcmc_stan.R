af7759140898ac209e9c855f37d0dc525a80a6d4
Johnny <37770895+johnnymdoubleu@users.noreply.github.com>
2023-11-06 11:12:39 +0000

    Update

--- simulation/mcmc/mcmc_stan.R
+++ simulation/mcmc/mcmc_stan.R
@@ -295,7 +295,7 @@ fit1 <- stan(
     init = init.alpha,      # initial value
     chains = 3,             # number of Markov chains
     warmup = 1000,          # number of warmup iterations per chain
-    iter = 2000,            # total number of iterations per chain
+    iter = 3000,            # total number of iterations per chain
     cores = 4,              # number of cores (could use one per chain)
     refresh = 500             # no progress shown
 )
