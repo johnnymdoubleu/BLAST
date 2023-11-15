0d1c72019206080a99340ec3b56a8e3c7a512f17
Johnny <37770895+johnnymdoubleu@users.noreply.github.com>
2023-11-15 15:32:16 +0000

    Update
    For scenario B, set.seed(50)

--- simulation/mcmc/mcmc_stan.R
+++ simulation/mcmc/mcmc_stan.R
@@ -22,8 +22,8 @@ library(cmdstanr)
 # library(ggplotify)
 
 #Scenario 1
-set.seed(2)
-# set.seed(50)
+# set.seed(2)
+set.seed(50)
 
 n <- 5000
 psi <- 20
@@ -242,7 +242,7 @@ model {
         target += pareto_lpdf(y[i] | u, alpha[i]);
     }
     target += gamma_lpdf(lambda1 | 0.1, 1);
-    target += gamma_lpdf(lambda2 | 0.1, 1);
+    target += gamma_lpdf(lambda2 | 0.1, 0.1);
     target += normal_lpdf(theta[1] | 0, 100);
     target += inv_gamma_lpdf(sigma | 0.01, 0.01); // target += double_exponential_lpdf(theta[1] | 0, lambda1)
     target += (p * log(lambda1) + (p * psi * log(lambda2)));
