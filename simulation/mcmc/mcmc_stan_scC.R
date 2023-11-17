8a660065359b6f730ea42b94a4c05cb650c4dac4
Johnny <37770895+johnnymdoubleu@users.noreply.github.com>
2023-11-17 22:04:35 +0000

    Update mcmc_stan_scC.R

--- simulation/mcmc/mcmc_stan_scC.R
+++ simulation/mcmc/mcmc_stan_scC.R
@@ -28,12 +28,72 @@ set.seed(36)
 
 n <- 5000
 psi <- 20
+threshold <- 0.90
+p <- 10
+no.theta <- 1
+simul.no <- 50
+
+xholder.nonlinear <- xholder.linear <- bs.nonlinear <- bs.linear <- matrix(,nrow=n, ncol=0)
+x.origin <- cbind(replicate(p, runif(n, 0, 1)))
+for(i in 1:p){
+    knots <- seq(min(x.origin[,i]), max(x.origin[,i]), length.out = psi)  
+    tps <- basis.tps(x.origin[,i], knots, m = 2, rk = FALSE, intercept = FALSE)
+    bs.linear <- cbind(bs.linear, tps[,1:no.theta])
+    bs.nonlinear <- cbind(bs.nonlinear, tps[,-c(1:no.theta)])  
+}
+
+gamma.origin <- matrix(, nrow = psi, ncol = p)
+for(j in 1:p){
+    for (ps in 1:psi){
+        if(j %in% c(2,4,5,6,9,10)){gamma.origin[ps, j] <- 0}
+        else if(j==7){
+            if(ps <= (psi/2)){gamma.origin[ps, j] <- 1}
+            else{gamma.origin[ps, j] <- 1}
+        }
+        else {
+            if(ps <= (psi/2)){gamma.origin[ps, j] <- 1}
+            else{gamma.origin[ps, j] <- 1}
+        }
+    }
+}
+
+theta.origin <- c(-0.1, 0.8, 0, 0.8, 0, 0, 0, -0.3, 0.8, 0, 0)
+
+n <- 5000
+psi <- 20
 threshold <- 0.9
 p <- 5
 no.theta <- 1
 simul.no <- 50
 
 xholder.nonlinear <- xholder.linear <- bs.nonlinear <- bs.linear <- matrix(,nrow=n, ncol=0)
+
+sample_meanvector <- runif(p,0,1)
+sample_covariance_matrix <- matrix(NA, nrow = p, ncol = p)
+diag(sample_covariance_matrix) <- 1
+
+mat_Sim <- matrix(data = NA, nrow = p, ncol = p)
+U <- runif(n = p) * 0.5
+
+for(i in 1:p)
+{
+  if(i %in% c(2,3))
+  {
+    U_Star <- pmin(U + 0.2 * runif(n = p), 0.99999)
+    
+  }else
+  {
+    U_Star <- pmin(pmax(U + sample(c(0, 1), size = p, replace = TRUE) * runif(n = p), 0.00001), 0.99999)
+  }
+  
+  mat_Sim[, i] <- qnorm(U_Star)  
+}
+
+cor_Mat <- cor(mat_Sim)
+sample_covariance_matrix <- cor_Mat * (p/2)
+# diag(sample_covariance_matrix) <- 1
+## create multivariate normal distribution
+# x.origin <- mvrnorm(n = n, mu = rep(0,p), Sigma = sample_covariance_matrix)
 
 C <- matrix(c(1, 0.3, 0.5, 0.3, 0.3,
               0.3, 1, 0.95, 0.4, 0.4,
@@ -87,7 +147,7 @@ for(i in 1:n){
 u <- quantile(y.origin, threshold)
 x.origin <- x.origin[which(y.origin>u),]
 # x.bs <- x.origin
-x.origin <- scale(x.origin)
+# x.origin <- scale(x.origin)
 y.origin <- y.origin[y.origin > u]
 n <- length(y.origin)
 
@@ -184,7 +244,7 @@ model {
     }
     target += gamma_lpdf(lambda1 | 1, 10);
     target += gamma_lpdf(lambda2 | 0.1, 0.1);
-    target += normal_lpdf(theta[1] | 0, 10);
+    target += normal_lpdf(theta[1] | 0, 1);
     target += inv_gamma_lpdf(sigma | 0.01, 0.01); // target += double_exponential_lpdf(theta[1] | 0, lambda1)
     target += (newp * log(lambda1) + (p * psi * log(lambda2)));
     for (j in 1:p){
