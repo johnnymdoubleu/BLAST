7d502208e2c24831d7ea2e84e36124f1f5223ae0
Johnny <37770895+johnnymdoubleu@users.noreply.github.com>
2023-11-17 17:08:51 +0000

    Update

--- simulation/mcmc/mcmc_stan.R
+++ simulation/mcmc/mcmc_stan.R
@@ -59,7 +59,7 @@ for(j in 1:p){
 
 theta.origin <- c(-0.1, 0.8, 0, 0.8, 0, 0, 0, -0.3, 0.8, 0, 0)
 
-n <- 20000
+n <- 5000
 psi <- 20
 threshold <- 0.9
 p <- 5
@@ -129,7 +129,7 @@ for(j in 1:p){
     }
 }
 
-theta.origin <- c(5, 0, -0.2, -0.2, 0, 0)
+theta.origin <- c(3, 0, -0.2, -0.2, 0, 0)
 
 f.nonlinear.origin <- f.linear.origin <- f.origin <- matrix(, nrow = n, ncol = p)
 for(j in 1:p){
@@ -244,7 +244,7 @@ model {
     }
     target += gamma_lpdf(lambda1 | 1, 10);
     target += gamma_lpdf(lambda2 | 0.1, 0.1);
-    target += normal_lpdf(theta[1] | 0, 1);
+    target += normal_lpdf(theta[1] | 0, 10);
     target += inv_gamma_lpdf(sigma | 0.01, 0.01); // target += double_exponential_lpdf(theta[1] | 0, lambda1)
     target += (newp * log(lambda1) + (p * psi * log(lambda2)));
     for (j in 1:p){
