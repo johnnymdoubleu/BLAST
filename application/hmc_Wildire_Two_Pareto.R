b1935d445ee4fbb3870832403f2f5ed1c6db34e2
Johnny <37770895+johnnymdoubleu@users.noreply.github.com>
2024-01-17 21:55:01 +0000

    Update hmc_Wildire_Two_Pareto.R

--- application/hmc_Wildire_Two_Pareto.R
+++ application/hmc_Wildire_Two_Pareto.R
@@ -204,7 +204,7 @@ model {
     target += gamma_lpdf(lambda1 | 1, 10);
     target += gamma_lpdf(lambda2 | 0.1, 0.1);
     target += normal_lpdf(theta[1] | 0, 1);
-    target += unif_lpdf(delta | 0, 10);
+    target += uniform_lpdf(delta | 0, 10);
     target += inv_gamma_lpdf(sigma | 0.01, 0.01); // target += double_exponential_lpdf(theta[1] | 0, lambda1)
     target += (newp * log(lambda1) + (p * psi * log(lambda2)));
     for (j in 1:p){
