091d3c469b5cca2d70e5085c0032c0c0bf8a4e30
Johnny <37770895+johnnymdoubleu@users.noreply.github.com>
2024-01-24 00:16:44 +0000

    Update hmc_Wildfire_summer.R

--- application/hmc_Wildfire_summer.R
+++ application/hmc_Wildfire_summer.R
@@ -86,7 +86,7 @@ fwi.index$month <- factor(format(fwi.index$date,"%b"),
 
 Y <- Y[which(fwi.index$month %in% c("Jun", "Jul", "Aug", "Sep"))]
 u <- quantile(Y, threshold)
-y <- Y[Y>u]/10
+y <- Y[Y>u] * 0.1
 fwi.scaled <- fwi.scaled[which(fwi.index$month %in% c("Jun", "Jul", "Aug", "Sep")),]
 
 fwi.scaled <- fwi.scaled[which(Y>u),]
@@ -234,38 +234,25 @@ set_cmdstan_path(path = NULL)
 # here using the example model that comes with CmdStan
 file <- file.path(cmdstan_path(), "model.stan")
 
-# init.alpha <- list(list(gamma = array(rep(0, (psi*p)), dim=c(psi, p)),
-#                         theta = rep(0, (p+1)), 
-#                         tau = rep(0.1, p), sigma = 0.1, 
-#                         lambda1 = 0.1, lambda2 = 0.1),
-#                   list(gamma = array(rep(0.02, (psi*p)), dim=c(psi, p)),
-#                         theta = rep(0.01, (p+1)), 
-#                         tau = rep(0.01, p), sigma = 0.001,
-#                         lambda1 = 0.01, lambda2 = 0.1),
-#                   list(gamma = array(rep(0.01, (psi*p)), dim=c(psi, p)),
-#                         theta = rep(0.05, (p+1)), 
-#                         tau = rep(0.01, p), sigma = 0.01,
-#                         lambda1 = 0.1, lambda2 = 0.01))
-
 init.alpha <- list(list(gamma = array(rep(0, (psi*p)), dim=c(psi, p)),
                         theta = rep(0, (p+1)), 
-                        tau = rep(1, p), sigma = 1, 
+                        tau = rep(0.1, p), sigma = 0.1, 
                         lambda1 = 0.1, lambda2 = 0.1),
                   list(gamma = array(rep(0.02, (psi*p)), dim=c(psi, p)),
                         theta = rep(0.01, (p+1)), 
-                        tau = rep(1, p), sigma = 1,
+                        tau = rep(0.01, p), sigma = 0.001,
                         lambda1 = 0.01, lambda2 = 0.1),
                   list(gamma = array(rep(0.01, (psi*p)), dim=c(psi, p)),
                         theta = rep(0.05, (p+1)), 
-                        tau = rep(1, p), sigma = 1,
+                        tau = rep(0.01, p), sigma = 0.01,
                         lambda1 = 0.1, lambda2 = 0.01))
 
 # stanc("C:/Users/Johnny Lee/Documents/GitHub/BRSTIR/application/model1.stan")
 fit1 <- stan(
     file = "model_pareto.stan",  # Stan program
     data = data.stan,    # named list of data
-    init = init.alpha,      # initial value
-    # init_r = 1,
+    # init = init.alpha,      # initial value
+    init_r = 1,
     chains = 3,             # number of Markov chains
     warmup = 1500,          # number of warmup iterations per chain
     iter = 3000,            # total number of iterations per chain
