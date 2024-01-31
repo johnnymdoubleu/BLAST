52c47db49893a01e7194d8daf2afa4d50c052361
Johnny <37770895+johnnymdoubleu@users.noreply.github.com>
2024-01-31 17:00:32 +0000

    Update

--- simulation/mcmc/hmc_stan_scA.R
+++ simulation/mcmc/hmc_stan_scA.R
@@ -372,7 +372,7 @@ ggplot(data.smooth, aes(x=x, group=interaction(covariates, replicate))) +
         axis.title.x = element_text(size = 35),
         axis.text = element_text(size=18))
 
-ggsave(paste0("./simulation/results/",Sys.Date(),"_",n,"_mcmc_smooth_sc1-wi.pdf"), width=12.5, height = 15)
+# ggsave(paste0("./simulation/results/",Sys.Date(),"_",n,"_mcmc_smooth_sc1-wi.pdf"), width=12.5, height = 15)
 
 data.linear <- data.frame("x"=newx,
                           "true" = as.vector(f.linear.new),
