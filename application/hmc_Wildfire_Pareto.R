a23502ecd40b38083fed6ab77fac36cd0527ccdf
Johnny <37770895+johnnymdoubleu@users.noreply.github.com>
2024-02-12 18:16:55 +0000

    Update hmc_Wildfire_Pareto.R

--- application/hmc_Wildfire_Pareto.R
+++ application/hmc_Wildfire_Pareto.R
@@ -43,7 +43,7 @@ df.long[which(is.na(df.long$...1))+1,]
 Y <- df.long$measurement[!is.na(df.long$measurement)]
 summary(Y) #total burnt area
 length(Y)
-psi <- 5
+psi <- 20
 threshold <- 0.95
 u <- quantile(Y, threshold)
 y <- Y[Y>u]
