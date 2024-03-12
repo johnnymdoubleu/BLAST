70a9d96dc7ef81d611fbf1d27edab946b89d6ce0
Johnny <37770895+johnnymdoubleu@users.noreply.github.com>
2024-03-12 13:17:24 +0000

    Update compare_loo.R

--- application/compare_loo.R
+++ application/compare_loo.R
@@ -1,8 +1,11 @@
 library(loo)
-
+setwd("C:/Users/Johnny Lee/Documents/GitHub")
 threshold = 0.95
 load(paste0("./BRSTIR/application/BRTIR_",Sys.Date(),"_",floor(threshold*100),"quantile_IC.Rdata"))
 load(paste0("./BRSTIR/application/BRSTIR_",Sys.Date(),"_",floor(threshold*100),"quantile_IC.Rdata"))
+load(paste0("./BRSTIR/application/BRSTIR_constraint_",Sys.Date(),"_",floor(threshold*100),"quantile_IC.Rdata"))
 
 loo_compare(brtir.elpd.loo, brstir.elpd.loo)
+loo_compare(constraint.elpd.loo, brstir.elpd.loo)
 loo_compare(brtir.waic, brstir.waic)
+loo_compare(constraint.waic, brstir.waic)
