7229b4ef706858146c164609953395b04ed99fd8
Johnny <37770895+johnnymdoubleu@users.noreply.github.com>
2024-03-12 22:09:56 +0000

    Update

--- application/hmc_constraint_Pareto.R
+++ application/hmc_constraint_Pareto.R
@@ -308,7 +308,7 @@ model {
     for (i in 1:n){
         target += pareto_lpdf(y[i] | u, alpha[i]);
     }
-    target += gamma_lpdf(lambda1 | 10, 0.1);
+    target += gamma_lpdf(lambda1 | 0.1, 0.01);
     target += gamma_lpdf(lambda2 | 0.1, 0.1);
     target += normal_lpdf(theta[1] | 0, 1);
     // target += normal_lpdf(theta[1] | log(1.2), 0.01) target += double_exponential_lpdf(theta[1] | 0, lambda1)
