9a22d9de951c1b088b7ae0be064b2cac1ffdeca1
Johnny <37770895+johnnymdoubleu@users.noreply.github.com>
2024-04-30 00:02:56 +0100

    Update hmc_constraint_scA.R

--- simulation/mcmc/hmc_constraint_scA.R
+++ simulation/mcmc/hmc_constraint_scA.R
@@ -554,7 +554,7 @@ ggplot(data.scenario, aes(x=x)) +
 
 # ggsave(paste0("./simulation/results/",Sys.Date(),"_",n,"_mcmc_alpha_test_sc1-wi.pdf"), width=10, height = 7.78)
 
-save(data.smooth, data.scenario, file = (paste0("./simulation/sweavedata.Rdata")))
+# save(data.smooth, data.scenario, file = (paste0("./simulation/sweavedata.Rdata")))
 
 mcmc.alpha <- posterior$alpha
 len <- dim(mcmc.alpha)[1]
