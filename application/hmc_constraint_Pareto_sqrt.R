65caaadcf63cf6151bdd15adfa506bb406a7240b
Johnny <37770895+johnnymdoubleu@users.noreply.github.com>
2024-05-22 12:25:45 +0100

    Update

--- application/hmc_constraint_Pareto_sqrt.R
+++ application/hmc_constraint_Pareto_sqrt.R
@@ -48,8 +48,8 @@ Y <- df.long$measurement[!is.na(df.long$measurement)]
 
 summary(Y) #total burnt area
 length(Y)
-psi <- 20
-threshold <- 0.99
+psi <- 10
+threshold <- 0.95
 u <- quantile(Y, threshold)
 y <- Y[Y>u]
 # x.scale <- x.scale[which(y>quantile(y, threshold)),]
