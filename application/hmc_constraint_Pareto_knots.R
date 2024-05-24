61f277018b63de41d51bd931c4117b8e5d45d1be
Johnny <37770895+johnnymdoubleu@users.noreply.github.com>
2024-05-24 09:46:09 +0100

    Update

--- application/hmc_constraint_Pareto_knots.R
+++ application/hmc_constraint_Pareto_knots.R
@@ -43,8 +43,8 @@ Y <- df.long$measurement[!is.na(df.long$measurement)]
 
 summary(Y) #total burnt area
 length(Y)
-psi <- 20
-threshold <- 0.98
+psi <- 30
+threshold <- 0.99
 u <- quantile(Y, threshold)
 y <- Y[Y>u]
 # x.scale <- x.scale[which(y>quantile(y, threshold)),]
