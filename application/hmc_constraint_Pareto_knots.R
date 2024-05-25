0aee4d36faa7eaf90d8271b181f7a2085e582f43
Johnny <37770895+johnnymdoubleu@users.noreply.github.com>
2024-05-25 22:33:36 +0100

    Update hmc_constraint_Pareto_knots.R

--- application/hmc_constraint_Pareto_knots.R
+++ application/hmc_constraint_Pareto_knots.R
@@ -11,7 +11,7 @@ library(qqboxplot)
 library(ggdensity)
 library(ggforce)
 library(ggdist)
-source("C:/Users/Johnny Lee/Documents/Github/extremis/R/bltir.R")
+# source("C:/Users/Johnny Lee/Documents/Github/extremis/R/bltir.R")
 
 options(mc.cores = parallel::detectCores())
 
@@ -86,7 +86,7 @@ fwi.index$date <- as.numeric(fwi.index$date)
 fwi.index$year <- substr(as.Date(cov.long$condition[missing.values], "%Y"),1,4)
 
 
-fit <- bltir(Y, fwi.scaled, threshold = 0.975, psi = 10, T = 2000)
+# fit <- bltir(Y, fwi.scaled, threshold = 0.975, psi = 10, T = 2000)
 
 u <- quantile(Y, threshold)
 y <- Y[Y>u]
@@ -225,93 +225,6 @@ generated quantities {
 "
 , "model_BRSTIR_constraint.stan")
 
-stanmod <- "data {
-    int <lower=1> n; // Sample size
-    int <lower=1> p; // regression coefficient size
-    int <lower=1> psi; // splines coefficient size
-    real <lower=0> u; // large threshold value
-    matrix[n,p] bsLinear; // fwi dataset
-    matrix[n, (psi*p)] bsNonlinear; // thin plate splines basis
-    matrix[n,p] xholderLinear; // fwi dataset
-    matrix[n, (psi*p)] xholderNonlinear; // thin plate splines basis    
-    vector[n] y; // extreme response
-    real <lower=0> atau;
-    matrix[2, (2*p)] basisFL;
-    array[(p*2)] int indexFL;
-}
-parameters {
-    vector[(p+1)] theta; // linear predictor
-    vector[(psi-2)] gammaTemp[p]; // constraint splines coefficient from 2 to psi-1
-    real <lower=0> lambda1; // lasso penalty
-    real <lower=0> lambda2; // group lasso penalty
-    array[p] real <lower=0> tau1;
-    array[p] real <lower=0> tau2;
-}
-transformed parameters {
-    array[n] real <lower=0> alpha; // covariate-adjusted tail index
-    vector[psi] gamma[p]; // splines coefficient 
-    vector[2] gammaFL[p];
-    real <lower=0> lambda2o;
-    matrix[n, p] gsmooth; // linear component
-
-
-    lambda2o=lambda2*100;
-    for(j in 1:p){
-        gamma[j][2:(psi-1)] = gammaTemp[j][1:(psi-2)]*100;
-        gammaFL[j] = basisFL[, (((j-1)*2)+1):(((j-1)*2)+2)] * (bsNonlinear[indexFL[(((j-1)*2)+1):(((j-1)*2)+2)], (((j-1)*psi)+2):(((j-1)*psi)+(psi-1))] * gammaTemp[j]*100) * (-1);
-        gamma[j][1] = gammaFL[j][1];
-        gamma[j][psi] = gammaFL[j][2];
-    };
-
-    for (j in 1:p){
-        gsmooth[,j] = bsNonlinear[,(((j-1)*psi)+1):(((j-1)*psi)+psi)] * gamma[j] + bsLinear[,j] * theta[j+1];
-    };
-
-    for (i in 1:n){
-        alpha[i] = exp(theta[1] + sum(gsmooth[i,]));
-    };
-}
-
-model {
-    // likelihood
-    for (i in 1:n){
-        target += pareto_lpdf(y[i] | u, alpha[i]);
-    }
-    target += normal_lpdf(theta[1] | 0, 100);
-    target += gamma_lpdf(lambda1 | 1, 1e-3);
-    target += gamma_lpdf(lambda2o | 1, 1e-3);
-    target += (2*p*log(lambda2o));
-    for (j in 1:p){
-        target += gamma_lpdf(tau1[j] | 1, lambda1^2*0.5);
-        target += normal_lpdf(theta[(j+1)] | 0, sqrt(1/tau1[j]));
-        target += gamma_lpdf(tau2[j] | atau, lambda2o^2*0.5);
-        target += multi_normal_lpdf(gamma[j] | rep_vector(0, psi), diag_matrix(rep_vector(1, psi)) * (1/tau2[j]));
-    }
-}
-generated quantities {
-    // Used in Posterior predictive check    
-    vector[n] log_lik;
-    vector[n] f;
-    real yrep;
-    array[n] real <lower=0> newalpha; // new tail index
-    matrix[n, p] newgsmooth; // linear component    
-
-    for (j in 1:p){
-        newgsmooth[,j] = xholderNonlinear[,(((j-1)*psi)+1):(((j-1)*psi)+psi)] * gamma[j] + xholderLinear[,j] * theta[j+1];
-    };    
-
-    for (i in 1:n){ 
-        newalpha[i] = exp(theta[1] + sum(newgsmooth[i,]));
-    };    
-
-    yrep = pareto_rng(u, alpha[1]); 
-    for(i in 1:n){
-      f[i] = pareto_rng(u, alpha[i]);
-      log_lik[i] = pareto_lpdf(y[i] | u, alpha[i]);
-    }
-}
-"
-
 data.stan <- list(y = as.vector(y), u = u, p = p, n= n, psi = psi, 
                     atau = ((psi+1)/2), indexFL = as.vector(t(index.holder)),
                     bsLinear = bs.linear, bsNonlinear = bs.nonlinear,
@@ -332,8 +245,7 @@ init.alpha <- list(list(gammaTemp = array(rep(0, ((psi-2)*p)), dim=c(p, (psi-2))
 
 # stanc("C:/Users/Johnny Lee/Documents/GitHub/BRSTIR/application/model1.stan")
 fit1 <- stan(
-    # file = "model_BRSTIR_constraint.stan",  # Stan program
-    model_code = stanmod,
+    file = "model_BRSTIR_constraint.stan",  # Stan program
     data = data.stan,    # named list of data
     init = init.alpha,      # initial value
     # init_r = 1,
