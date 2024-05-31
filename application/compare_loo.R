1e525b4760a3290a4fe9df99c86124690175c94d
Johnny <37770895+johnnymdoubleu@users.noreply.github.com>
2024-05-31 11:00:57 +0100

    Update compare_loo.R

--- application/compare_loo.R
+++ application/compare_loo.R
@@ -4,15 +4,19 @@ threshold <- 0.97
 date <- "2024-05-23"
 load(paste0("./BRSTIR/application/BRTIR_",date,"_",floor(threshold*100),"quantile_IC.Rdata"))
 # load(paste0("./BRSTIR/application/BRSTIR_",Sys.Date(),"_",floor(threshold*100),"quantile_IC.Rdata"))
-load(paste0("./BRSTIR/application/BRSTIR_constraint_",date,"_",10,"_",floor(threshold*100),"quantile_IC.Rdata"))
+load(paste0("./BRSTIR/application/BRSTIR_constraint_",date,"_",5,"_",floor(threshold*100),"quantile_IC.Rdata"))
 constraint.elpd.loo.5 <- constraint.elpd.loo
-load(paste0("./BRSTIR/application/BRSTIR_constraint_",date,"_",30,"_",floor(threshold*100),"quantile_IC.Rdata"))
+load(paste0("./BRSTIR/application/BRSTIR_constraint_",date,"_",10,"_",floor(threshold*100),"quantile_IC.Rdata"))
 constraint.elpd.loo.10 <- constraint.elpd.loo
-load(paste0("./BRSTIR/application/BRSTIR_constraint_",date,"_",40,"_",floor(threshold*100),"quantile_IC.Rdata"))
+load(paste0("./BRSTIR/application/BRSTIR_constraint_",date,"_",20,"_",floor(threshold*100),"quantile_IC.Rdata"))
 constraint.elpd.loo.20 <- constraint.elpd.loo
+load(paste0("./BRSTIR/application/BRSTIR_constraint_",date,"_",30,"_",floor(threshold*100),"quantile_IC.Rdata"))
+constraint.elpd.loo.30 <- constraint.elpd.loo
+load(paste0("./BRSTIR/application/BRSTIR_constraint_",date,"_",40,"_",floor(threshold*100),"quantile_IC.Rdata"))
+constraint.elpd.loo.40 <- constraint.elpd.loo
 
 
-compare <- loo_compare(constraint.elpd.loo.5, constraint.elpd.loo.10, constraint.elpd.loo.20, brtir.elpd.loo)
+compare <- loo_compare(constraint.elpd.loo.5, constraint.elpd.loo.10, constraint.elpd.loo.20,constraint.elpd.loo.30, constraint.elpd.loo.40, brtir.elpd.loo)
 print(compare, simplify = FALSE)
 
 # loo_compare(brtir.elpd.loo, brstir.elpd.loo)
