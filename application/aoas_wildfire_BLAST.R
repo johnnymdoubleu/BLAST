a1d513af7da8bd3c2761823809ea7c2f949606cd
Johnny <37770895+johnnymdoubleu@users.noreply.github.com>
2025-05-26 17:59:02 -0400

    Update aoas_wildfire_BLAST.R

--- application/aoas_wildfire_BLAST.R
+++ application/aoas_wildfire_BLAST.R
@@ -279,7 +279,7 @@ fit1 <- stan(
     data = data.stan,    # named list of data
     init = init.alpha,      # initial value
     chains = 3,             # number of Markov chains
-    iter = 10000,            # total number of iterations per chain
+    iter = 40000,            # total number of iterations per chain
     cores = parallel::detectCores(), # number of cores (could use one per chain)
     refresh = 2000           # no progress shown
 )
